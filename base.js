9b3ed24175f1a41e93787fac506a7eb861a3f8f5
Alex Madeira <alex.c.madeira@gmail.com>
2024-02-18 15:11:43 -0300

    fix(base): add comments to identify sorted import blocks

--- base.js
+++ base.js
@@ -18,31 +18,40 @@ module.exports = {
       'error',
       {
         groups: [
+          /* ---------------Types */
           ['^.*\\u0000$'], // type Imports
           ['^@/'], // imports starting with @/ (normally @types folder)
+          /* ---------------Tests */
           ['^tests/', '^__tests/', '^__tests__/'], // Imports starting with tests, __tests or __tests__ (normally tests folder)
 
+          /* ---------------Common modules */
           ['^node:'], // Node modules Imports
           ['^react'], // React Imports
           ['^styled-components'], // Styled Components Imports
           ['^next'], // NextJS Imports
 
+          /* ---------------Packages */
           ['^@?\\w'], // Other Import Packages
           ['^\\u0000'], // Side effect Imports.
 
+          /* ---------------Docs */
           ['^#/'], // Imports starting with #/ (normally Docs folder)
 
+          /* ---------------Internal Folders */
           ['^_UTL/'], // Imports starting with _UTL/ (normally utils folder)
           ['^_SRV/'], // Imports starting with _SRV/ (normally Services folder)
           ['^_STR/'], // Imports starting with _STR/ (normally Store folder)
           ['^_MDL/'], // Imports starting with _MDL/ (normally Model folder)
           ['^_CTL/'], // Imports starting with _CTL/ (normally Controller folder)
 
+          /* ---------------SRC */
           ['^~/'], // Imports starting with ~/ (normally src folder)
 
+          /* ---------------Parent Imports */
           ['^\\.\\.(?!/?$)', '^\\.\\./?$'], // Parent Imports (../)
           ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'], // Same-folder Imports (./)
 
+          /* ---------------Style imports */
           ['^.+\\.s?css$'], // global CSS or SCSS Imports (style.css, path/to/file.scss)
           ['^\\./styles$'], //  Same-folders style Import (./styles)
         ],
